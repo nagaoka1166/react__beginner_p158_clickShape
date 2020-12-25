5a6a0fde32fe756619cd7c1e0eeace8c46627117
Ryunosuke Nagoka <69971830+nagaoka1166@users.noreply.github.com>
2020-12-25 23:40:59 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, {Component } from 'react';
+import Rect from './Rect'
+import './App.css';
+
+class App extends Component {
+  input = '';
+
+msgStyle = {
+    fontSize: "20pt",
+    color: "#900",
+    margin: "20px 0px",
+    padding: "5px",
+    
+}
+ 
+
+
+
+constructor(props) {
+    super(props);
+    this.state = {
+    message: 'type your name:'
+    };
+   
+    this.doChange = this.doChange.bind(this);
+    this.doSubmit = this.doSubmit.bind(this);
+    }
+
+doChange(event) {
+    this.input = event.target.value;
+}
+
+doSubmit(event) {
+    this.setState({
+        message: 'Hello,' + this.input+ '!!'
+    });
+    event.preventDefault();
+}
+
+
+
+render () {
+    return
+        <div>
+        <h1>React</h1>
+        <Message title="Children!">
+        これはコンポーネント内のコンテンツです。
+　　　　マルでテキストを分割し、リストに表示します。
+    　　開業は必要ありません。
+      </Message>
+    
+    </div>;
+                                 
+}
+}
+
+
+
+class Message extends Component {
+    
+    li = {
+        fontSize: "16pt",
+        color: "#06",
+        margin: "0px",
+        padding: "opx",
+    }
+
+    redner() {
+        let  content = this.props.children;
+        let arr = content.split('。');
+        let arr2 = [];
+        for(let i = 0;i <arr.length;i++) {
+            arr2.push(arr[i]);
+        }
+        
+        let list = arr2.map((value,key) => (
+             <li style={this.li} key={key}>{value}.</li> 
+        )
+        
+        );
+        return 
+          <div>
+         <h2>{this.props.title}</h2>
+            <ol>{list}</ol>
+          </div>
+      
+        
+        
+    }
+    
+}
+
+
+
+
+ export default App;
+
