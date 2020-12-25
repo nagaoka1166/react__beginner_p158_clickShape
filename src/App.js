c95e1a048cf7d7b6e7050277d746e84cafcb6fe2
Ryunosuke Nagoka <69971830+nagaoka1166@users.noreply.github.com>
2020-12-25 23:42:20 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,95 +3,73 @@ import Rect from './Rect'
 import './App.css';
 
 class App extends Component {
-  input = '';
+    data = [];
 
-msgStyle = {
-    fontSize: "20pt",
-    color: "#900",
-    margin: "20px 0px",
-    padding: "5px",
-    
+ msgStyle = {
+     fontSize: "20pt",
+     color: "#900",
+     margin: "20px 0px",
+     padding: "5px",
+ }
+
+area = {
+    width: "500px",
+    height: "500px",
+    border: "1px solid  blue"
 }
- 
 
 
 
 constructor(props) {
     super(props);
     this.state = {
-    message: 'type your name:'
+    list:this.data
     };
-   
-    this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
+   this.doAction = this.doAction.bind(this);
     }
-
-doChange(event) {
-    this.input = event.target.value;
-}
-
-doSubmit(event) {
+doAction(e) {
+    let x =e.pageX;
+    let y =e.pageY;
+    this.data.push({x:x, y:y});
     this.setState({
-        message: 'Hello,' + this.input+ '!!'
-    });
-    event.preventDefault();
+       list:this.data
+    
+     });
+    
 }
 
-
-
-render () {
-    return
-        <div>
+//図形を表示
+draw(d) {
+    let s = {
+        position: "absolute",
+//        クリックした所から(d.〇 - 25)離れたところに図形表示
+        left:(d.x - 25) + "px",　　　　　　
+        top:(d.y - 25) + "px",
+        width: "50px",
+        height: "50px",
+        backgroundColor: "#66f3",
+    };
+    
+    return <div style={s}></div>;
+    
+}
+render(){
+    return <div>
         <h1>React</h1>
-        <Message title="Children!">
-        これはコンポーネント内のコンテンツです。
-　　　　マルでテキストを分割し、リストに表示します。
-    　　開業は必要ありません。
-      </Message>
-    
+//   
+   
+        <h2 style={this.msgStyle}>show rect.</h2>
+     
+        <div style= {this.area} onClick={this.doAction}>
+            {this.data.map((value)=>this.draw(value))}
+        </div>
+   
     </div>;
-                                 
-}
-}
-
-
-
-class Message extends Component {
     
-    li = {
-        fontSize: "16pt",
-        color: "#06",
-        margin: "0px",
-        padding: "opx",
-    }
-
-    redner() {
-        let  content = this.props.children;
-        let arr = content.split('。');
-        let arr2 = [];
-        for(let i = 0;i <arr.length;i++) {
-            arr2.push(arr[i]);
-        }
-        
-        let list = arr2.map((value,key) => (
-             <li style={this.li} key={key}>{value}.</li> 
-        )
-        
-        );
-        return 
-          <div>
-         <h2>{this.props.title}</h2>
-            <ol>{list}</ol>
-          </div>
-      
-        
-        
-    }
     
 }
 
-
-
+}
 
  export default App;
 
